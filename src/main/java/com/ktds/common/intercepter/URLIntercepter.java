7d3e7fa40401c63ff480e09eea85aaf92d145f44
이근재 <hanzen927@gmail.com>
2016-11-03 14:04:07 +0900

    1

--- src/main/java/com/ktds/common/intercepter/URLIntercepter.java
+++ src/main/java/com/ktds/common/intercepter/URLIntercepter.java
@@ -1,8 +1,11 @@
 package com.ktds.common.intercepter;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.loader.ParallelWebappClassLoader;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -14,7 +17,13 @@ public class URLIntercepter extends HandlerInterceptorAdapter{
 		
 		System.out.println("클라이언트가 접속함!");
 		System.out.println(request.getRemoteAddr());//client IP
-		
+		PrintWriter out = response.getWriter();
+		out.write("<script>										");
+		out.write("alert('interceptor');												");
+		out.write("												");
+		out.write("</script>											");
+		out.flush();
+		out.close();
 		// true를 리턴할 경우 컨트롤러에게 요청을 넘긴다.
 		// false를 리천할 경우 컨트롤러에게 요청을 넘기지 않고 응답처리 한다.
 		return true;
